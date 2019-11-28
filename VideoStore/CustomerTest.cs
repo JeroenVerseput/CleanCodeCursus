20b5ff2fde4f5cfb2ae886bde7d14716a92839ff
Jeroen Verseput <jeroen.verseput@topicus.nl>
2019-11-28 16:12:47 +0100

    hardfix culture for test

--- VideoStore/CustomerTest.cs
+++ VideoStore/CustomerTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Threading;
 using NUnit.Framework;
 
 namespace VideoStore
@@ -8,8 +10,14 @@ namespace VideoStore
     [TestFixture]
     public class CustomerTest
     {
+		[SetUp]
+	    public void SetUp()
+	    {
+		    Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+		}
+
         [Test]
-        public void characterizationTest()
+        public void CharacterizationTest()
         {
             Customer customer = new Customer("John Doe");
             customer.addRental(new Movie("Star Wars", Movie.NEW_RELEASE), 6);
@@ -18,8 +26,8 @@ namespace VideoStore
 
             String expected = "Rental Record for John Doe\n"
                               + "	Star Wars	18\n"
-                              + "	Sofia	7,5\n"
-                              + "	Inception	6,5\n"
+                              + "	Sofia	7.5\n"
+                              + "	Inception	6.5\n"
                               + "Amount owed is 32\n"
                               + "You earned 4 frequent renter points";
 
--- VideoStoreClean/StatementFormatterTest.cs
+++ VideoStoreClean/StatementFormatterTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Threading;
 using NUnit.Framework;
 
 namespace VideoStoreClean
@@ -8,8 +10,13 @@ namespace VideoStoreClean
     [TestFixture]
     public class StatementFormatterTest
     {
+	    [SetUp]
+	    public void SetUp()
+	    {
+		    Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+	    }
 
-        [Test]
+		[Test]
         public void characterizationTest()
         {
             List<Rental> rentals = new List<Rental>
@@ -21,8 +28,8 @@ namespace VideoStoreClean
 
             String expected = "Rental Record for John Doe\n"
                               + "	Star Wars	18\n"
-                              + "	Sofia	7,5\n"
-                              + "	Inception	6,5\n"
+                              + "	Sofia	7.5\n"
+                              + "	Inception	6.5\n"
                               + "Amount owed is 32\n"
                               + "You earned 4 frequent renter points";
 
